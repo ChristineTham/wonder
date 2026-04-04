c1daf0ff311379adeb5d85b416daa28190e0c3f3
Christine Tham <chris.tham@hellotham.com>
2026-04-04 18:46:39 +1100

    Moved from self hoisted font awesome to astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import alpine from '@astrojs/alpinejs';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
         alpine({
             entrypoint: '/src/scripts/alpine.js'
-        })
+        }),
+        icon()
     ]
 });
